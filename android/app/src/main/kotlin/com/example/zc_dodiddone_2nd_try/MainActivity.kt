1423db73e726ba371ea71df938fb51617684d1ce
PlokhikhD <plokhikh.dm@gmail.com>
2024-07-28 22:56:50 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/zc_dodiddone_2nd_try/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zc_dodiddone_2nd_try
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:zc_dodiddone_2nd_try/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
